48580a22b41ac1427f7dcb648830f975439b20db
Your Name <tahanymo7amed@yahoo.com>
2020-01-20 15:27:54 +0200

    form

--- server/app.js
+++ server/app.js
@@ -1,12 +1,11 @@
 const express= require("express")
 const mongoose=require("mongoose")
-<<<<<<< HEAD
 const badyparser=require("body-parser")
 const cors=require("cors")
 const product=require("./controllers/product")
-const route = express.Router();
+
 const app=express();
-const fs = require("fs")
+
 const PORT=3000;
 app.use(badyparser.json());
 app.use(cors());
@@ -22,15 +21,13 @@ app.post('/enrollment',function(req,res){ console.log(req.body);
     }); 
 
 
-=======
-const cors=require("cors")
+
 const fs = require("fs")
 // const fileUpload = require('express-fileupload')
 const product=require("./controllers/product")
 const route = express.Router();
 const app=express();
 // app.use(fileUpload());
->>>>>>> 9408a28fae4ae655af2af7a1e64ae2dcb08b68d9
 
 mongoose.connect("mongodb://127.0.0.1:27017/MagicShop",(err,res)=>{
     if(err){
@@ -43,19 +40,11 @@ mongoose.connect("mongodb://127.0.0.1:27017/MagicShop",(err,res)=>{
 app.use("/get",product)
 app.use(express.static("public"));   
 
-<<<<<<< HEAD
- app.use(function(req,resp,next){
-     resp.setHeader("Access-Control-Allow-Origin","*");
-     resp.setHeader("Access-Control-Allow-Methods","GET,POST,PUT,DELETE");
-    next()
-   });
-=======
 app.use(function(req,resp,next){
     resp.setHeader("Access-Control-Allow-Origin","*");
     resp.setHeader("Access-Control-Allow-Methods","GET,POST,PUT,DELETE");
     next()
   });
->>>>>>> 9408a28fae4ae655af2af7a1e64ae2dcb08b68d9
 
   var files_arr=fs.readdirSync(__dirname+"/models")
   files_arr.forEach(function(file){
@@ -63,11 +52,6 @@ app.use(function(req,resp,next){
   });
 
 
-<<<<<<< HEAD
-app.listen(PORT,function(){
-    console.log("server is listenning ...."+PORT)
-=======
 app.listen(4200,function(){
     console.log("server is listenning ....")
->>>>>>> 9408a28fae4ae655af2af7a1e64ae2dcb08b68d9
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,6 @@ import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-import { TdfformComponent } from './nav/tdfform/tdfform.component';
-import { HttpClientModule } from '@angular/common/http';
-import { TdfsignupComponent } from './tdfsignup/tdfsignup.component';
-@NgModule({
-  declarations: [
-    AppComponent,
-    TdfformComponent,
-    TdfsignupComponent
-=======
 import { ProductsComponent } from './products/products.component';
 import { BagsComponent } from './bags/bags.component';
 import{HttpClientModule} from '@angular/common/http';
@@ -23,22 +13,15 @@ import {AllProductService}from '../services/all-product.service';
     AppComponent,
     ProductsComponent,
     BagsComponent
->>>>>>> 9408a28fae4ae655af2af7a1e64ae2dcb08b68d9
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-<<<<<<< HEAD
-    FormsModule,
-   HttpClientModule 
-   
-=======
     HttpClientModule,
     
   ],
   providers: [
     AllProductService
->>>>>>> 9408a28fae4ae655af2af7a1e64ae2dcb08b68d9
   ],
   bootstrap: [AppComponent]
 })
