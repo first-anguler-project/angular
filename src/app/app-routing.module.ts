f6c9225f05bed2bc69d7084a1a053e3f7afd26e7
nahla <nahla.barakat1997.com>
2020-01-22 03:14:10 +0200

    bags category and detail page design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BagsComponent } from "src/app/bags/bags.component";
+import { BagsCategoryComponent } from "src/app/bags-category/bags-category.component";
 
 
 const routes: Routes = [
-  {path:"details" ,component:BagsComponent}
+  {path:"details" ,component:BagsComponent},
+  {path:"Bags" ,component:BagsCategoryComponent}
 ];
 
 @NgModule({
