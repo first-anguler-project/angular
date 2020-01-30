b6de53bab740ab4aa522122acc6e01fe59f29f40
nahla <nahla.barakat1997.com>
2020-01-30 20:48:10 +0200

    responsive slider

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,16 +7,13 @@ import { AllProductService } from 'src/services/all-product.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-<<<<<<< HEAD
   public showdress=[];
   public showMakeup=[];
   public bags=[];
  // public allproducts=[];
   constructor(private getall:AllProductService,private getproduct:AllProductService) { }
-=======
 public allproduct=[];
-  constructor() { }
->>>>>>> 4b62a865cf8145557675ae7dcecf20191800a5f0
+ 
 
   ngOnInit() {
     this.getall.getdress().subscribe(data=>{
