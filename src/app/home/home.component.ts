f6e3a391305cd682f20ecaaff43d092559adbc52
tahany2020 <59687190+tahany2020@users.noreply.github.com>
2020-01-30 14:26:36 +0200

    slider

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AllProductService } from 'src/services/all-product.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  public showdress=[];
+  public showMakeup=[];
+  public bags=[];
+ // public allproducts=[];
+  constructor(private getall:AllProductService,private getproduct:AllProductService) { }
 
   ngOnInit() {
-  }
+    this.getall.getdress().subscribe(data=>{
+      console.log(data)
+      this.showdress=data
+  })
+  this.getall.getmakeup().subscribe(data=>{
+    console.log(data)
+    this.showMakeup=data
+   
+   } )
+   this.getproduct.getproduct().subscribe(data =>
+    this.bags=data
+  );
 
+  
+}
 }
